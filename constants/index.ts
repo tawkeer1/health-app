6ce82716ff8f8e919d67089356f03ff0e46a6e97
tawkeer1 <towqeerahmad111@gmail.com>
2024-09-08 12:19:02 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+export const GenderOptions = ["Male", "Female", "Other"];
+
+export const PatientFormDefaultValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  birthDate: new Date(Date.now()),
+  gender: "Male" as Gender,
+  address: "",
+  occupation: "",
+  emergencyContactName: "",
+  emergencyContactNumber: "",
+  primaryPhysician: "",
+  insuranceProvider: "",
+  insurancePolicyNumber: "",
+  allergies: "",
+  currentMedication: "",
+  familyMedicalHistory: "",
+  pastMedicalHistory: "",
+  identificationType: "",
+  identificationNumber: "",
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
+};
+
+export const IdentificationTypes = [
+  "Aadhar Card",
+  "PAN Card",
+  "Birth Certificate",
+  "Driver's License",
+  "Medical Insurance Card/Policy",
+  "Army Id card",
+  "Passport",
+  "Social Security Card",
+  "State ID Card",
+  "Student ID Card",
+  "Voter ID Card",
+];
+
+export const Doctors = [
+  {
+    image: "/assets/images/dr-green.png",
+    name: "Abdul Majeed",
+  },
+  {
+    image: "/assets/images/dr-cameron.png",
+    name: "Shaista Gani",
+  },
+  {
+    image: "/assets/images/dr-livingston.png",
+    name: "David Livingston",
+  },
+  {
+    image: "/assets/images/dr-peter.png",
+    name: "Rahul Kumar",
+  },
+  {
+    image: "/assets/images/dr-powell.png",
+    name: "Jane Powell",
+  },
+  {
+    image: "/assets/images/dr-remirez.png",
+    name: "Alex Ramirez",
+  },
+  {
+    image: "/assets/images/dr-lee.png",
+    name: "Jasmine Lee",
+  },
+  {
+    image: "/assets/images/dr-cruz.png",
+    name: "Alyana Cruz",
+  },
+  {
+    image: "/assets/images/dr-sharma.png",
+    name: "Hardik Sharma",
+  },
+];
+
+export const StatusIcon = {
+  scheduled: "/assets/icons/check.svg",
+  pending: "/assets/icons/pending.svg",
+  cancelled: "/assets/icons/cancelled.svg",
+};
